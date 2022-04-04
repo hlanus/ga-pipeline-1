968343fdc483760f1a9362221cb7690275d6e4d5
hlanus <hlanus@asu.edu>
2022-04-03 17:59:34 -0700

    Add a support for sbatch in our trimmomatic script

---
+++ scripts/submit_trim_jobs.bash
@@ -0,0 +1,9 @@
+metadata=data/untrimmed_fastq/data_urls.txt
+outdir=data/trimmed_fastq
+infiles=$(cut -d' ' -f 1 $metadata | grep _1.fastq.gz)
+indir=data/untrimmed_fastq
+outdir=data/trimmed_fastq
+
+for filename in $infiles; do
+  sbatch scripts/trimmomatic.bash $indir/$filename $outdir
+done
